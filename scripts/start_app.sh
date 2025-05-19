2c23ced662f02beb9caee17a08bfe406b5eaeb0c
yuvwwa <150217655+yuvwwa@users.noreply.github.com>
2025-05-20 01:37:45 +0800

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-# Скрипт для запуска приложения, генерирующего логи (filebeat)
+# Скрипт для запуска приложения, генерирующего логи (logging_app)
 
 # Активируем виртуальное окружение, если оно используется
 source ~/venv/bin/activate
 
-# Очищаем предыдущий файл логов
-rm -f ~/elk/app/elvis.json
+# Перезапускаем компоненты ELK
+echo "Запуск Elasticsearch..."
+sudo systemctl start elasticsearch.service
+sudo systemctl status elasticsearch.service
 
-# Запускаем приложение для генерации логов
-cd ~/elk/app
-python3 elvis.py
+echo "Запуск Kibana..."
+sudo systemctl start kibana.service
+sudo systemctl status kibana.service
+
+echo "Запуск Logstash..."
+sudo systemctl start logstash.service
+sudo systemctl status logstash.service
+
+echo "Kibana доступна по адресу: http://84.201.151.216:5601"
+
+echo "Установим зависимости..."
+pip install python-logstash-async
+pip install fastapi
+pip install uvicorn
+
+echo "Запустим наше мини приложение"
+uvicorn logging_app:app --host 0.0.0.0 --port 8000
